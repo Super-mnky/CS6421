06c55409c9f765c396b5b0b06ee41392298754e7
root <root@localhost.localdomain>
2018-09-19 14:24:27 -0400

    added useful comments and simple error handling

--- PythonClient.py
+++ PythonClient.py
@@ -1,40 +1,39 @@
-
-# CS 6421 - Simple Message Board Client in Python
-# T. Wood
-# Run with:     python msgclient.py
+# A simple client to interact with the  KVstore server.
+# Written by Jerry Jones IV
 
 import socket
 import argparse
 import sys
 import select
 
+# to enable input commands, we add sys.argvs to catch and assignthem to vars
 host=sys.argv[1] if len(sys.argv) > 1 else "somevalue"
-#userName=sys.argv[2] if len(sys.argv) > 1 else "somevalue2"
 userData=sys.argv[2] if len(sys.argv) > 1 else "somevalue3"
 
-
-# host = "localhost";
+#as specified by server, we connect on port 5555
 portnum = 5555;
 
 #create an INET, STREAMing socket
 s = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM)
-#now connect to the web server on port 80
-# - the normal http port
 
+#connect and send the message
 s.connect ((host,5555))
 s.send(userData)
 
+#we always send message first, but where appropriate we recieve data also
 while True:
     
+    #find what sockets are avaiable 
     socket_list = [sys.stdin, s]
     ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
 
+    #for all sockets read and print the data
     for sock in ready_to_read:
             if sock == s:
-                # incoming message from remote server, s
                 data = sock.recv(4096)
                 print data
+                #if blank, kill the client
                 if not data :
                     print 'Disconnected from chat server'
                     sys.exit()
--- PythonServer.py
+++ PythonServer.py
@@ -1,9 +1,15 @@
+# This is a KV pair server.
+# Writte by Jerry Jones IV
+
 #!/usr/bin/env python3
 import hashlib
 import socket
 
-HOST = ''  # Standard loopback interface address (localhost)
-PORT = 5555        # Port to listen on (non-privileged ports are > 1023)
+
+#standard loopback interface and arbirarily chosen port 5555, initialize KVstore
+HOST = ''          
+PORT = 5555        
+KVstore = {}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -11,48 +17,51 @@ s.listen(0)
 
 print ("Now listening for connections...")
 
-KVstore = {}
-
 while True:
     conn, addr = s.accept()
     print ('Connected by', addr)
+    
+    try:
+        #recieve user submitted command then parse it on 'spaces' to work with the items individually
+        data = conn.recv(1024)
+        clientInfo = str.split(data)
+        operation = clientInfo[0]
 
-    data = conn.recv(1024)
-    clientInfo = str.split(data)
+        if (operation.lower() == 'stats'):
+            entries = ((str(len(KVstore))))
+            conn.send("\nThe number of entries is: " + entries)
 
-    operation = clientInfo[0]
-    #key = (hashlib.md5(clientInfo[2])).digest()
+        elif (operation.lower() == 'set'):
+            key = clientInfo[1]
+            value = clientInfo[2]
+            KVstore[key] = value
+            conn.send("\nSET command succesfully executed.")
 
-    if (operation.lower() == 'stats'):
-        entries = ((str(len(KVstore))))
-        conn.send("\nThe number of entries is: " + entries)
+        #for multi KV pairs, once we've logged 1 pair, loop through info incrementing by 2 to always catch the KV pairs
+        elif (operation.lower() == 'multiset'):
+            index = 1
+            while index < len(clientInfo):
+                KVstore[clientInfo[index]] = clientInfo[index+1]
+                index = index + 2
+            conn.send("\nMultiset command succesfully executed.")
 
-    if (operation.lower() == 'set'):
-        key = clientInfo[1]
-        value = clientInfo[2]
-        KVstore[key] = value
-        conn.send("\nSET command succesfully executed.")
-
-    if (operation.lower() == 'multiset'):
-        index = 1
-        while index < len(clientInfo):
-            KVstore[clientInfo[index]] = clientInfo[index+1]
-            index = index + 2
-        conn.send("\nMultiset command succesfully executed.")
-
-    if (operation.lower() == 'get'):
-        key = clientInfo[1]
-        keyResponse = KVstore.get(key)
-        conn.send("\nThe value for '" + key + "' is: " + keyResponse)
+        elif (operation.lower() == 'get'):
+            key = clientInfo[1]
+            keyResponse = KVstore.get(key)
+            conn.send("\nThe value for '" + key + "' is: " + keyResponse)
         
-    if (operation.lower() == 'multiget'):
-        index = 1
-        while index < len(clientInfo):
-            keyResponse = KVstore.get(clientInfo[index])
-            conn.send("\nThe value for '" + clientInfo[index]  + "' is: " + keyResponse)
-            index = index + 1
-        conn.send("\n\nMultiget command succesfully executed.")
+        #same as multiset
+        elif (operation.lower() == 'multiget'):
+            index = 1
+            while index < len(clientInfo):
+                keyResponse = KVstore.get(clientInfo[index])
+                conn.send("\nThe value for '" + clientInfo[index]  + "' is: " + keyResponse)
+                index = index + 1
+            conn.send("\n\nMultiget command succesfully executed.")
 
+    #here to catch invalid operations or incorrect formatting of KV pairs
+    except:
+        conn.send("\nImproper operation or formatting. Terminating session.")
 
     conn.close()
     print
